88e4eac3fd74c9d3d2cf1dc1c098aeff1108e4fa
Aaleia <aaleia.fernando@gmail.com>
2024-08-22 12:49:49 -0400

    Added suggestions

--- myapp/src/components/MainContent.jsx
+++ myapp/src/components/MainContent.jsx
@@ -4,18 +4,31 @@ import "./../components/MainContent.css";
 
 class MainContent extends Component {
   render() {
-    return (
-      <div className="text-input">
-        <input
-          style={{backgroundColor: "#D6CAE3" }}
-          type="text"
-          placeholder="Where can I find..."
-        />
-        <button class="send-button" style={{ backgroundColor: "#2F124F" }}>
-          <i style={{ color: "#FFC72A" }} class="far fa-paper-plane"></i>
-        </button>
-      </div>
-    );
+      return (
+	  <div className="main-content">
+	      
+	  <div className="suggestions">
+              <button className="suggestion-button" style={{ backgroundColor: "#ffc72a" }}>
+		  Suggestion 1
+              </button>
+	      <button className="suggestion-button" style={{ backgroundColor: "#ffc72a" }}>
+		  Suggestion 2
+              </button>
+	  </div>
+	
+	  <div className="text-input">
+              <input
+		  style={{backgroundColor: "#D6CAE3" }}
+		  type="text"
+		  placeholder="Where can I find..."
+              />
+              <button className="send-button" style={{ backgroundColor: "#2F124F" }}>
+		  <i style={{ color: "#FFC72A" }} className="far fa-paper-plane"></i>
+              </button>
+	  </div>
+	      
+	  </div>
+      );
   }
 }
 
